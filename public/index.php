2f6327f6a21b0881873f2c87bd3a4b38c2c68a48
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-12-13 21:15:22 +0100

    Updated index.php

--- public/index.php
+++ public/index.php
@@ -4,16 +4,55 @@ require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATO
 use Cactus\App;
 use Cactus\Http\HttpCode;
 use Cactus\Routing\Router;
+use Cactus\Template\Pass\EchoPass;
+use Cactus\Template\Pass\I18nPass;
+use Cactus\Template\TemplateManager;
+use Cactus\Util\ClientRequest;
 
 try {
-    $config = null;
+    $config = json_decode(file_get_contents(ASSET_PATH . 'config.json'), true, 512, JSON_THROW_ON_ERROR);
+
+    $request = ClientRequest::Instance();
 
     $router = new Router();
+    $templateEngine = new TemplateManager();
 
     $app = new App($config);
+
+    $templateEngine->addPass(new EchoPass());
+    $templateEngine->addPass(new I18nPass());
+    $templateEngine->registerTemplate("layout");
+
+    $router->get("error", "/error/:error{[1-5]\d{2}}", $templateEngine);
+    $templateEngine->registerTemplate("error");
+
+    $parameters = [];
+    $route = $router->resolveRoute(
+        $request->getUrl(),
+        $request->getMethod(),
+        $parameters
+    );
+    echo $route->call($parameters);
+
 } catch (Throwable $e) {
 
-    http_response_code(HttpCode::SERVER_ERROR);
-    require_once VIEWS_PATH . "fallback.html";
+    $httpCode = $e->getCode();
+    if (!HttpCode::isHttpCode($httpCode))
+        $httpCode = HttpCode::SERVER_ERROR;
+
+    try {
+
+        http_response_code($httpCode);
+        $parameters = [];
+        $route = $router->resolveRoute("error/" . $httpCode, "GET", $parameters);
+        echo $route->call($parameters);
+
+    } catch (Throwable $e) {
+
+        http_response_code(HttpCode::SERVER_ERROR);
+        $fallback = file_get_contents(VIEWS_PATH . "fallback.html");
+        $exceptionDetails = ini_get("display_errors") ? "<hr/>" . nl2br($e->getTraceAsString()) : null;
+        echo str_replace("%{exception.details}", $exceptionDetails, $fallback);
+    }
 
 }
