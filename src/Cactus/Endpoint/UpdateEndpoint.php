da43ac5c7305e82a5162736b2f1f861c6cba619d
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2020-01-07 17:57:19 +0100

    Tentatively fixed the update process

--- src/Cactus/Endpoint/UpdateEndpoint.php
+++ src/Cactus/Endpoint/UpdateEndpoint.php
@@ -13,57 +13,66 @@ class UpdateEndpoint implements IRouteEndpoint
     private const APP_URL = "https://github.com/TheWhoosher/Cactus/archive/master.zip";
 
     private string $archiveFile;
+    private ZipArchive $archive;
 
     /**
      * @inheritDoc
      */
     public function handle(Route $route, array $parameters): string
     {
-        $tmpDir = \sys_get_temp_dir();
-        $this->archiveFile = \tempnam($tmpDir, "Cactus");
+        $tmpDir = sys_get_temp_dir();
+        $this->archiveFile = tempnam($tmpDir, "Cactus");
 
         $options = array(
-            \CURLOPT_URL => self::APP_URL,
-            \CURLOPT_RETURNTRANSFER => true,
-            \CURLOPT_BINARYTRANSFER => true,
-            \CURLOPT_FOLLOWLOCATION => true,
-            \CURLOPT_USERAGENT => "Cactus",
-            \CURLOPT_CONNECTTIMEOUT => 60,
-            \CURLOPT_TIMEOUT => 60
+            CURLOPT_URL => self::APP_URL,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_BINARYTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_USERAGENT => "Cactus",
+            CURLOPT_CONNECTTIMEOUT => 60,
+            CURLOPT_TIMEOUT => 60
         );
-        $ch = \curl_init();
-        \curl_setopt_array($ch, $options);
-        $archiveData = \curl_exec($ch);
+        $ch = curl_init();
+        curl_setopt_array($ch, $options);
+        $archiveData = curl_exec($ch);
 
         if (!$archiveData) {
-            $error = \curl_error($ch);
-            \curl_close($ch);
+            $error = curl_error($ch);
+            curl_close($ch);
             return $this->release("Unable to download the latest version of Cactus: " . $error);
         }
 
-        \curl_close($ch);
+        curl_close($ch);
 
         if (!file_put_contents($this->archiveFile, $archiveData)) {
             return $this->release("Unable to save the archive to the disk.");
         }
 
-        $zip = new ZipArchive();
-        if (!$zip->open($this->archiveFile)) {
-            return $this->release("Unable to open the zip archive. The download is probably corrupted");
+        $this->archive = new ZipArchive();
+        if (!$this->archive->open($this->archiveFile)) {
+            return $this->release("Unable to open the zip archive. The download is probably corrupted.");
         }
 
-        if (!$zip->extractTo(ROOT)) {
-            $zip->close();
+        $destination = tempnam($tmpDir, 'Cactus');
+        if (!$this->archive->extractTo($destination)) {
             return $this->release("Unable to extract the zip archive. It's probably permission-related?");
         }
 
-        $zip->close();
+        if (!rename($destination, ROOT)) {
+            return $this->release("Unable to move the up-to-date application to the Web directory.");
+        }
+
         return $this->release("Cactus is now up-to-date");
     }
 
-    private function release(string $error): string
+    private function release(string $message): string
     {
-        \unlink($this->archiveFile);
-        return $error;
+        if (file_exists($this->archiveFile))
+            unlink($this->archiveFile);
+
+        if (isset($this->archive))
+            $this->archive->close();
+
+        return $message;
     }
 }
