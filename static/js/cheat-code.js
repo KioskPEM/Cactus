89859433ecd0189281cb33711726cbc5deb9636b
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2020-01-09 15:37:05 +0100

    admin password

--- static/js/cheat-code.js
+++ static/js/cheat-code.js
@@ -5,7 +5,7 @@ const CheatCode = {
 
         document.addEventListener("keydown", CheatCode.handleKeyPress);
 
-        CheatCode.register("ADMPASS", function () {
+        CheatCode.register("PASSWORD", function () {
             let adminPage = document.getElementById("admin-page");
             window.location.href = adminPage.value;
         });
