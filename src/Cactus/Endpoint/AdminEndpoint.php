a7d7bd5a9fc6dbbe76e1b2784c63eaf0c6b64e20
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2020-01-09 13:53:29 +0100

    Tentativelity trying to print some text

--- src/Cactus/Endpoint/AdminEndpoint.php
+++ src/Cactus/Endpoint/AdminEndpoint.php
@@ -6,6 +6,8 @@ use Cactus\Http\HttpCode;
 use Cactus\Routing\Exception\RouteException;
 use Cactus\Routing\IRouteEndpoint;
 use Cactus\Routing\Route;
+use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
+use Mike42\Escpos\Printer;
 
 class AdminEndpoint implements IRouteEndpoint
 {
@@ -32,6 +34,18 @@ class AdminEndpoint implements IRouteEndpoint
                 return "<a href=\"$this->updateUrl\" onclick=\"this.text = 'Cactus is now updating...'\">Click here to update Cactus.</a>";
             case "quit":
                 return system("cmd /c C:\Cactus\stop.bat");
+            case "print":
+
+                $text = $_GET['text'] ?? "";
+                $com = $_GET['com'] ?? "COM1";
+                $connector = new WindowsPrintConnector($com);
+                $printer = new Printer($connector);
+                $printer->text($text);
+                $printer->barcode("po;;e", Printer::BARCODE_CODE39);
+                $printer->cut();
+                $printer->close();
+                return "PRINTED_OR_IS_IT?";
+
             default:
                 throw new RouteException("Invalid action", HttpCode::CLIENT_BAD_REQUEST);
         }
