aab3453cf29983e577b17d407268a331125d07ba
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2020-01-10 14:25:20 +0100

    Updated ticket format

--- src/Cactus/Endpoint/PrintTicketEndpoint.php
+++ src/Cactus/Endpoint/PrintTicketEndpoint.php
@@ -40,20 +40,21 @@ class PrintTicketEndpoint implements IRouteEndpoint
         $printer->setTextSize(2, 2);
         $printer->text("Pierre Emile Martin");
 
-        $printer->feed(2);
+        $printer->setJustification(Printer::JUSTIFY_LEFT);
+        $printer->setTextSize(1, 1);
+        $printer->feed(10);
 
         $firstName = $parameters["first_name"] ?? "John";
         $lastName = $parameters["last_name"] ?? "Doe";
 
-        $printer->setJustification(Printer::JUSTIFY_LEFT);
-        $printer->setTextSize(1, 1);
         $printer->text("Bienvenue, ");
         $printer->text($firstName);
         $printer->text(" ");
         $printer->text($lastName);
-        $printer->feed();
+        $printer->feed(10);
 
         $id = $parameters["id"] ?? "0123456789";
+        $printer->setBarcodeHeight(4);
         $printer->barcode($id, Printer::BARCODE_CODE39);
 
         $printer->cut(Printer::CUT_PARTIAL);
