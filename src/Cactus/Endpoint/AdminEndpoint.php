a5bbad06bf32549397151f246930f92d6e883f28
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2020-01-13 13:55:10 +0100

    Removed unused action

--- src/Cactus/Endpoint/AdminEndpoint.php
+++ src/Cactus/Endpoint/AdminEndpoint.php
@@ -7,8 +7,6 @@ use Cactus\Routing\Exception\RouteException;
 use Cactus\Routing\IRouteEndpoint;
 use Cactus\Routing\Route;
 use Cactus\Util\AppConfiguration;
-use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
-use Mike42\Escpos\Printer;
 
 class AdminEndpoint implements IRouteEndpoint
 {
@@ -25,30 +23,6 @@ class AdminEndpoint implements IRouteEndpoint
                 return "<a href=\"$updateUrl\" onclick=\"this.text = 'Cactus is now updating...'\">Click here to update Cactus.</a>";
             case "quit":
                 return system("cmd /c C:\Cactus\stop.bat");
-            case "print":
-
-                $text = $_GET['text'] ?? "test";
-                $com = $_GET['com'] ?? "COM1";
-                $code = $_GET['code'] ?? "POMME";
-                $qrCode = $_GET['qrcode'] ?? "false";
-
-                $connector = new WindowsPrintConnector($com);
-                $printer = new Printer($connector);
-                $printer->initialize();
-                $printer->setJustification(Printer::JUSTIFY_CENTER);
-                $printer->text($text);
-                $printer->feed();
-
-                if ($qrCode === "true")
-                    $printer->qrCode($code, Printer::QR_ECLEVEL_H, 16);
-                else
-                    $printer->barcode($code);
-
-                $printer->feed();
-                $printer->cut();
-                $printer->close();
-                return "PRINTED_OR_IS_IT?";
-
             default:
                 throw new RouteException("Invalid action", HttpCode::CLIENT_BAD_REQUEST);
         }
