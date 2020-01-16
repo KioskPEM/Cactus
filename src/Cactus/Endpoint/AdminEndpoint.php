d8bebed94919eaf2cd30e009a5b436162a345b7e
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2020-01-16 22:08:24 +0100

    RM: Removed update action ADD: Added clear_cache action

--- src/Cactus/Endpoint/AdminEndpoint.php
+++ src/Cactus/Endpoint/AdminEndpoint.php
@@ -6,7 +6,6 @@ use Cactus\Http\HttpCode;
 use Cactus\Routing\Exception\RouteException;
 use Cactus\Routing\IRouteEndpoint;
 use Cactus\Routing\Route;
-use Cactus\Util\AppConfiguration;
 
 class AdminEndpoint implements IRouteEndpoint
 {
@@ -18,13 +17,17 @@ class AdminEndpoint implements IRouteEndpoint
         $action = $parameters["action"];
 
         switch ($action) {
-            case "update":
-                $updateUrl = AppConfiguration::get("url.root") . "/update.php";
-                return "<a href=\"$updateUrl\" onclick=\"this.text = 'Cactus is now updating...';this.href=''\">Click here to update Cactus.</a>";
+            case "clear_cache":
+                session_destroy();
+                break;
             case "quit":
-                return system("cmd /c C:\Cactus\stop.bat");
+                system("cmd /c C:\Cactus\stop.bat");
+                break;
             default:
                 throw new RouteException("Invalid action", HttpCode::CLIENT_BAD_REQUEST);
         }
+
+        http_response_code(HttpCode::SUCCESS_NO_CONTENT);
+        return "";
     }
 }
