a93e97d8177b0d36f11719b241001526a387ac6c
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2020-01-23 21:05:39 +0100

    Implemented pagination

--- src/Cactus/Controller/SearchSchool/SelectSchoolController.php
+++ src/Cactus/Controller/SearchSchool/SelectSchoolController.php
@@ -18,6 +18,27 @@ class SelectSchoolController implements ITemplateController
 
     private const SCHOOL_PER_PAGES = 8;
 
+    private array $schools;
+
+    private function ensureSchools($regionCode, $departmentCode, $schoolType): array
+    {
+        if (isset($this->schools))
+            return $this->schools;
+
+        $schoolDatabase = new CsvDatabase($schoolType, ';');
+        $schoolDatabase->open();
+        $this->schools = $schoolDatabase->get(function ($entry) use ($departmentCode, $regionCode) {
+            return $entry[self::REGION_CODE] === $regionCode && $entry[self::DEPARTMENT_CODE] === $departmentCode;
+        });
+        $schoolDatabase->close();
+
+        usort($this->schools, function ($a, $b) {
+            return $a[self::SCHOOL_NAME] <=> $b[self::SCHOOL_NAME];
+        });
+
+        return $this->schools;
+    }
+
     /**
      * @param RenderContext $context
      * @return string
@@ -29,30 +50,53 @@ class SelectSchoolController implements ITemplateController
         $departmentCode = $context->param("route.department");
         $schoolType = $context->param("route.school_type");
 
-        $schoolDatabase = new CsvDatabase($schoolType, ';');
-        $schoolDatabase->open();
-        $schools = $schoolDatabase->get(function ($entry) use ($departmentCode, $regionCode) {
-            return $entry[self::REGION_CODE] === $regionCode && $entry[self::DEPARTMENT_CODE] === $departmentCode;
-        });
-        $schoolDatabase->close();
-
-        usort($schools, function ($a, $b) {
-            return $a[self::SCHOOL_NAME] <=> $b[self::SCHOOL_NAME];
-        });
+        $this->ensureSchools($regionCode, $departmentCode, $schoolType);
 
         $output = "";
 
         $page = intval($context->param("route.page"));
         $offset = $page * self::SCHOOL_PER_PAGES;
-        $schoolCount = count($schools);
+        $schoolCount = count($this->schools);
         for ($i = 0; $i < self::SCHOOL_PER_PAGES && (($offset + $i) < $schoolCount); $i++) {
-            $school = $schools[$offset + $i];
+            $school = $this->schools[$offset + $i];
 
             $url = $context->buildUrl("GET", "sign-up", [
                 "school_id" => $school[self::SCHOOL_ID]
             ]);
             $schoolName = $school[self::SCHOOL_NAME];
             $output .= "<li class=\"grid-list-item\"><a class=\"button\" href=\"$url\">$schoolName</a></li>";
+        }
+
+        return $output;
+    }
+
+    /**
+     * @param RenderContext $context
+     * @return string
+     * @throws RouteNotFoundException
+     */
+    public function get_pagination(RenderContext $context): string
+    {
+        $regionCode = $context->param("route.region");
+        $departmentCode = $context->param("route.department");
+        $schoolType = $context->param("route.school_type");
+
+        $this->ensureSchools($regionCode, $departmentCode, $schoolType);
+
+        $output = "";
+
+        $schoolCount = count($this->schools);
+        $pageCount = ceil($schoolCount / self::SCHOOL_PER_PAGES);
+        for ($i = 0; $i < $pageCount; $i++) {
+
+            $url = $context->buildUrl("GET", "search-school.school", [
+                "region" => $regionCode,
+                "department" => $departmentCode,
+                "school_type" => $schoolType,
+                "page" => $i
+            ]);
+
+            $output .= "<li class='pagination-list-item'><a class=\"button\" href=\"$url\">$i</a></li>";
         }
 
         return $output;
