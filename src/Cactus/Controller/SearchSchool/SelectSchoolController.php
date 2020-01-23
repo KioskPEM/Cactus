13fd60244a0784aa213fd45963be382ef7c54d02
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2020-01-23 19:36:14 +0100

    Updated loading panel

--- src/Cactus/Controller/SearchSchool/SelectSchoolController.php
+++ src/Cactus/Controller/SearchSchool/SelectSchoolController.php
@@ -15,6 +15,8 @@ class SelectSchoolController implements ITemplateController
     private const DEPARTMENT_CODE = 10;
     private const REGION_CODE = 12;
 
+    private const SCHOOL_PER_PAGES = 8;
+
     public function get_schools(RenderContext $context): string
     {
         $regionCode = $context->param("route.region");
@@ -28,13 +30,22 @@ class SelectSchoolController implements ITemplateController
         });
         $schoolDatabase->close();
 
+        usort($schools, function ($a, $b) {
+            return $a[self::SCHOOL_NAME] <=> $b[self::SCHOOL_NAME];
+        });
+
         $output = "";
-        foreach ($schools as $school) {
+
+        $page = $context->param("route.page");
+        for ($i = $page; $i < self::SCHOOL_PER_PAGES; $i++) {
+            $school = $schools[$i * self::SCHOOL_PER_PAGES];
+
             $schoolId = $school[self::SCHOOL_ID];
             $url = "http://127.0.0.1:8080/public/index.php?lang=fr&path=sign-up/" . $schoolId;
             $schoolName = $school[self::SCHOOL_NAME];
             $output .= "<li class=\"grid-list-item\"><a class=\"button\" href=\"$url\">$schoolName</a></li>";
         }
+
         return $output;
     }
 }
