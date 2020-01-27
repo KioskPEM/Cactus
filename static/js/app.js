7bb6830f91d6e1615df46cf5d93d31c6b641101b
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2020-01-27 15:15:23 +0100

    Removed loading screen for now

--- static/js/app.js
+++ static/js/app.js
@@ -2,9 +2,9 @@ const Cactus = {
     idleDelay: 300,
     init: function () {
 
-        let links = document.getElementsByTagName("A");
+        /*let links = document.getElementsByTagName("A");
         for(let i = 0; i < links.length; i++)
-            links[i].addEventListener("click", Cactus.showLoadingPanel);
+            links[i].addEventListener("click", Cactus.showLoadingPanel);*/
 
         ["mousedown", "mousemove", "keypress", "scroll", "touchstart"].forEach(name => {
             document.addEventListener(name, Cactus.resetIdleDelay, true);
@@ -17,7 +17,7 @@ const Cactus = {
         if (welcomeBtn !== null)
             welcomeBtn.click();
     },
-    showLoadingPanel: function() {
+    showLoadingPanel: function () {
         let loadingPanel = document.getElementById("loading-panel");
         if (loadingPanel !== null)
             loadingPanel.style.display = "flex";
