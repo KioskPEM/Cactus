6502a73dcc14e896ec3d222076e9f28da76f13a6
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2020-02-14 18:53:04 +0100

    Updated default page

--- src/Cactus/Endpoint/AdminEndpoint.php
+++ src/Cactus/Endpoint/AdminEndpoint.php
@@ -11,9 +11,6 @@ use Cactus\Util\UrlBuilder;
 
 class AdminEndpoint implements IRouteEndpoint
 {
-    private const EXIT_CMD = "kill ";
-    private const START_CMD = "startx";
-
     /**
      * @inheritDoc
      */
@@ -22,13 +19,6 @@ class AdminEndpoint implements IRouteEndpoint
         $action = $parameters["action"];
 
         switch ($action) {
-            case "quit":
-                shell_exec(self::EXIT_CMD);
-                break;
-            case "restart";
-                shell_exec(self::EXIT_CMD);
-                shell_exec(self::START_CMD);
-                break;
             case "set_presentation_mode":
                 $config = AppConfiguration::Instance();
                 $config->set("home-page", "presentation.index");
