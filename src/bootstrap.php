8b42f7e97bf3e85a5dba324c956f03d37eacc7f5
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2020-02-27 17:47:24 +0100

    Updated class loading

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -17,14 +17,11 @@ spl_autoload_register(function ($className) {
     if ($lastNsPos = strrpos($className, '\\')) {
         $namespace = substr($className, 0, $lastNsPos);
         $className = substr($className, $lastNsPos + 1);
-        $fileName = SOURCE_PATH . str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . ".php";
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 
-    if (file_exists($fileName))
-        require_once $fileName;
-    else
-        throw new Exception("Class not found " . $className);
+    require $fileName;
 });
 
 set_exception_handler(function ($e) {
